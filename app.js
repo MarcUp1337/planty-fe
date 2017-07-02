26c65ca54e12c85b93ff485ff7b859b4f722cb57
Round Rabbit <roundrabbit@gmail.com>
2017-07-02 13:37:22 +0200

    initial

---
+++ app.js
@@ -0,0 +1,61 @@
+angular.module('plantsApp', []).controller('PlantCtrl', function($scope, $http){
+	var auth = window.btoa("admin:PlantsVsZombies");
+	
+	$http({
+		method: 'GET',
+		url: 'http://192.168.178.180:8080/plant/all',
+		headers: {
+		  'Content-Type':'application/json',
+		  'Authorization': 'basic' + auth
+		}
+	}).then(function(response) {
+		$scope.plants = response.data;
+	}, function(response) {
+		console.log('error',response)
+	});
+
+	$http({
+		method: 'GET',
+		url: 'http://192.168.178.180:8080/plant/0/',
+		headers: {
+			'Content-Type':'application/json',
+			'Authorization': 'basic' + auth
+		}
+	}).then(function(response) {
+		$scope.plant0 = response.data;
+		
+		
+		
+	}, function(response) {
+		console.log('error',response)
+	});
+
+	$http({
+		method: 'GET',
+		url: 'http://192.168.178.180:8080/plant/0/humidity',
+		headers: {
+			'Content-Type':'application/json',
+			'Authorization': 'basic' + auth
+		}
+	}).then(function(response) {
+		$scope.plant0.humidity = 100 - response.data;
+		getWaterRingChart("waterRing", getDegreeFromPercent(response.data));
+	}, function(response) {
+		console.log('error',response)
+	});
+	
+	$scope.water = function() {
+		$http({
+			method: 'GET',
+			url: 'http://192.168.178.180:8080/plant/0/water',
+			headers: {
+				'Content-Type':'application/json',
+				'Authorization': 'basic' + auth
+			}
+		}).then(function(response) {
+			console.log('watered',response)
+		}, function(response) {
+			console.log('error',response)
+		});
+	}
+});
