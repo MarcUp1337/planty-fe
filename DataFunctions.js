bd22b985c4c8f204b622e59ccc30dd96954729c0
Marc Lehmann <mln@dasburo.com>
2017-08-28 13:51:33 +0200

    format

--- DataFunctions.js
+++ DataFunctions.js
@@ -3,90 +3,98 @@ var OFFSET = 640;
 var MAX_VAL = 200;
 var CIRCLE = 360;
 
-function getLastValues(plant, number){
-	var length = waterlog.length;
-	var lastValues = {labels:[], values:[]};
-	
-	for(var i = number; i > 0; i--){
-		var index = length - i;
-		
-		if(index > 0){
-			lastValues.labels.push(waterlog[index].time);
-			lastValues.values.push(getDegreeFromSensor(waterlog[index].value));
-		}
-	}
-	return lastValues;
+function getLastValues(plant, number) {
+    var length = waterlog.length;
+    var lastValues = {labels: [], values: []};
+
+    for (var i = number; i > 0; i--) {
+        var index = length - i;
+
+        if (index > 0) {
+            lastValues.labels.push(waterlog[index].time);
+            lastValues.values.push(getDegreeFromSensor(waterlog[index].value));
+        }
+    }
+    return lastValues;
 }
 
-function getLastHourValues(plant, number){
-	var tenMinutes = getLastValues(plant, number * 6);
-	var length = tenMinutes.values.length;
-	var lastValues = {labels:[], values:[]};
-	
-	for(var i = number; i > 0; i--){
-		var index = length - i;
-		
-		if(index > 0){
-			lastValues.labels.push(waterlog[index].time);
-			lastValues.values.push(getDegreeFromSensor(waterlog[index].value));
-		}
-	}
-	return lastValues;
+function getLastHourValues(plant, number) {
+    var tenMinutes = getLastValues(plant, number * 6);
+    var length = tenMinutes.values.length;
+    var lastValues = {labels: [], values: []};
+
+    for (var i = number; i > 0; i--) {
+        var index = length - i;
+
+        if (index > 0) {
+            lastValues.labels.push(waterlog[index].time);
+            lastValues.values.push(getDegreeFromSensor(waterlog[index].value));
+        }
+    }
+    return lastValues;
 }
 
-function getDegreeFromSensor(value){
-	var newVal = value - OFFSET;
-	if(newVal < 0){newVal = 0;}
-	if(newVal > MAX_VAL){newVal = MAX_VAL;}
-	newVal = MAX_VAL - newVal;
-	newVal = newVal * CIRCLE / MAX_VAL;
-	return newVal;
+function getDegreeFromSensor(value) {
+    var newVal = value - OFFSET;
+    if (newVal < 0) {
+        newVal = 0;
+    }
+    if (newVal > MAX_VAL) {
+        newVal = MAX_VAL;
+    }
+    newVal = MAX_VAL - newVal;
+    newVal = newVal * CIRCLE / MAX_VAL;
+    return newVal;
 };
 
-function getDegreeFromPercent(value){
-	var newVal = value;
-	if(newVal < 0){newVal = 0;}
-	if(newVal > 100){newVal = 100;}
-	newVal = newVal * CIRCLE / 100;
-	return newVal;
+function getDegreeFromPercent(value) {
+    var newVal = value;
+    if (newVal < 0) {
+        newVal = 0;
+    }
+    if (newVal > 100) {
+        newVal = 100;
+    }
+    newVal = newVal * CIRCLE / 100;
+    return newVal;
 };
 
-function getPercentFromSensor(value){
-	var newVal = getDegreeFromSensor(value);
-	newVal = newVal / 3.6;
-	return Math.round(newVal, 0);
+function getPercentFromSensor(value) {
+    var newVal = getDegreeFromSensor(value);
+    newVal = newVal / 3.6;
+    return Math.round(newVal, 0);
 };
 
-function getSmoothed(values, cycles){
-	var newValues = values;
-	for(var cy = 1; cy <= cycles; cy++){
-		for (var i = 1; i < newValues.length -1; i++) {
-			var b = newValues[i-1];//before
-			var c = newValues[i];//current
-			var a = newValues[i+1];//after
-			
-			if(!(c > b && c < a) && !(c < b && c > a)){// c liegt nicht zwischen b und a
-				newValues[i] = ((a - b) / 2 ) + (b * 1);
-			}
-		}		
-	}
-	return newValues;
+function getSmoothed(values, cycles) {
+    var newValues = values;
+    for (var cy = 1; cy <= cycles; cy++) {
+        for (var i = 1; i < newValues.length - 1; i++) {
+            var b = newValues[i - 1];//before
+            var c = newValues[i];//current
+            var a = newValues[i + 1];//after
+
+            if (!(c > b && c < a) && !(c < b && c > a)) {// c liegt nicht zwischen b und a
+                newValues[i] = ((a - b) / 2 ) + (b * 1);
+            }
+        }
+    }
+    return newValues;
 }
 
-function getSquare(values, number){
-	var newValues = [];
-	var pos = 1;
-	var temp = 0;
-	
-	for(var i = 0; i < values.length; i++){
-		if(pos == number){
-			newValues.push(temp + values[i]);
-			temp = 0;
-			pos = 1;
-		} else {
-			temp += values[i];
-			pos++;
-		}
-	}
-	return newValues;
+function getSquare(values, number) {
+    var newValues = [];
+    var pos = 1;
+    var temp = 0;
+
+    for (var i = 0; i < values.length; i++) {
+        if (pos == number) {
+            newValues.push(temp + values[i]);
+            temp = 0;
+            pos = 1;
+        } else {
+            temp += values[i];
+            pos++;
+        }
+    }
+    return newValues;
 }
--- app.js
+++ app.js
@@ -1,58 +1,58 @@
-angular.module('plantApp', []).controller('PlantCtrl', function($scope, $http){
-	var auth = window.btoa("admin:PlantsVsZombies");
-	
-	$http({
-		method: 'GET',
-		url: 'http://raspberrypi:8080/plant/all',
-		headers: {
-		  'Content-Type':'application/json',
-		  'Authorization': 'basic' + auth
-		}
-	}).then(function(response) {
-		$scope.plants = response.data;
-	}, function(response) {
-		console.log('error',response)
-	});
+angular.module('plantApp', []).controller('PlantCtrl', function ($scope, $http) {
+    var auth = window.btoa("admin:PlantsVsZombies");
 
-	$http({
-		method: 'GET',
-		url: 'http://raspberrypi:8080/plant/0/',
-		headers: {
-			'Content-Type':'application/json',
-			'Authorization': 'basic' + auth
-		}
-	}).then(function(response) {
-		$scope.plant0 = response.data;
-	}, function(response) {
-		console.log('error',response)
-	});
+    $http({
+        method: 'GET',
+        url: 'http://raspberrypi:8080/plant/all',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'basic' + auth
+        }
+    }).then(function (response) {
+        $scope.plants = response.data;
+    }, function (response) {
+        console.log('error', response)
+    });
 
-	$http({
-		method: 'GET',
-		url: 'http://raspberrypi:8080/plant/0/humidity',
-		headers: {
-			'Content-Type':'application/json',
-			'Authorization': 'basic' + auth
-		}
-	}).then(function(response) {
-		$scope.plant0.humidity = 100 - response.data;
-		getWaterRingChart("waterRing", getDegreeFromPercent(response.data));
-	}, function(response) {
-		console.log('error',response)
-	});
-	
-	$scope.water = function() {
-		$http({
-			method: 'GET',
-			url: 'http://raspberrypi:8080/plant/0/water',
-			headers: {
-				'Content-Type':'application/json',
-				'Authorization': 'basic' + auth
-			}
-		}).then(function(response) {
-			console.log('watered',response)
-		}, function(response) {
-			console.log('error',response)
-		});
-	}
+    $http({
+        method: 'GET',
+        url: 'http://raspberrypi:8080/plant/0/',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'basic' + auth
+        }
+    }).then(function (response) {
+        $scope.plant0 = response.data;
+    }, function (response) {
+        console.log('error', response)
+    });
+
+    $http({
+        method: 'GET',
+        url: 'http://raspberrypi:8080/plant/0/humidity',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'basic' + auth
+        }
+    }).then(function (response) {
+        $scope.plant0.humidity = 100 - response.data;
+        getWaterRingChart("waterRing", getDegreeFromPercent(response.data));
+    }, function (response) {
+        console.log('error', response)
+    });
+
+    $scope.water = function () {
+        $http({
+            method: 'GET',
+            url: 'http://raspberrypi:8080/plant/0/water',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'basic' + auth
+            }
+        }).then(function (response) {
+            console.log('watered', response)
+        }, function (response) {
+            console.log('error', response)
+        });
+    }
 });
